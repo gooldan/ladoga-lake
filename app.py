4008cd254ace373b69f14cf968dcc5b24932b633
eschavelev <egor.schavelev@gmail.com>
2019-04-30 00:24:48 +0300

    fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)
+my_app = Flask(__name__)
 
 
 @app.route('/')
@@ -9,4 +9,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    my_app.run()
