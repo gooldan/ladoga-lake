c6822e26a47029c24e6c4f59e7d27a2ede9ed82d
eschavelev <egor.schavelev@gmail.com>
2019-04-30 03:12:24 +0300

    real

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
 import os
 import psycopg2
-from flask import Flask, render_template, g
+from psycopg2 import sql
+from flask import Flask, render_template, g, request, Response, redirect, url_for
 
 my_app = Flask(__name__)
+
+
+
 
 def connect_db():
     return psycopg2.connect(os.environ.get('DATABASE_URL'))
 
+
 @my_app.before_request
 def before_request():
     g.db_conn = connect_db()
+    g.db_conn.autocommit = True
+
+
+@my_app.route('/handle_measure', methods=['POST'])
+def handle_measure():
+    temp = float(request.form['measure_value'])
+    termo_id = int(request.form['termometer_id'])
+    strt = "insert into measure(id,termometer_id,temperature) values (DEFAULT,%d,%f)" % (termo_id, temp)
+    print(strt)
+    curs = g.db_conn.cursor()
+    curs.execute(sql.SQL(strt))
+    return redirect(url_for('index'))
+    # your code
+    # return a response
+
 
 @my_app.route('/')
 def index():
     cur = g.db_conn.cursor()
     cur.execute("SELECT * FROM termometer;")
-    return render_template('index.html', termometers=cur.fetchall())
+
+    #
+    cur_1 = g.db_conn.cursor()
+    cur_1.execute("SELECT * FROM measure;")
+    return render_template('index.html', termometers=cur.fetchall(), measures=cur_1.fetchall())
 
 
 if __name__ == '__main__':
---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+from app import my_app
+
+
+if __name__ == '__main__':
+    my_app.debug = True
+    my_app.run()
