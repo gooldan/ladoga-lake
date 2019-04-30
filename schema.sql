bec2a5d8b0e029232b02e908d0680f2a175a05f3
eschavelev <egor.schavelev@gmail.com>
2019-04-30 03:33:26 +0300

    last commit

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
-drop table if exists termometer;
+drop table if exists termometer CASCADE;
 create table termometer (
   id serial PRIMARY KEY,
   name text not null,
   lake_position text not null
 );
 
-drop table if exists measure;
+drop table if exists measure CASCADE;
 create table measure (
   id serial PRIMARY KEY,
   termometer_id integer REFERENCES termometer (id),
--- test_db.py
+++ test_db.py
@@ -7,9 +7,7 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 def test_db():
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM measure;")
-    for country in cur:
-        print(country)
+    cur.execute("insert into measure(id,termometer_id,temperature) values (DEFAULT,2,23.000000)")
 
     cur.close()
     conn.close()
